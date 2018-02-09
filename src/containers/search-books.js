1510064b96f6ddeb430820e8b37f6a3659f605b2
Chander <rc250348@ncr.com>
2018-02-09 12:38:05 +0530

    removed char length

--- src/containers/search-books.js
+++ src/containers/search-books.js
@@ -9,35 +9,41 @@ class SearchBooks extends Component{
     }
     
     searchBook = (query) => {
-        if(query.length > 2){
-            bookApi.search(query).then((res) => {
-                let shelfHistory = JSON.parse(localStorage.getItem('shelfHistory'));
-                let updatedResponse = res.map(book => {
-                    book.shelf='none'
-                    for(var count = 0; count < shelfHistory.length; count++){
-                        
-                        if(book.id === shelfHistory[count].id){
-                            book.shelf=shelfHistory[count].shelf;
-                        }
+        bookApi.search(query).then((res) => {
+            let shelfHistory = JSON.parse(localStorage.getItem('shelfHistory'));
+            let updatedResponse = res.map(book => {
+                book.shelf='none'
+                for(var count = 0; count < shelfHistory.length; count++){
+                    
+                    if(book.id === shelfHistory[count].id){
+                        book.shelf=shelfHistory[count].shelf;
                     }
-                    return book
-                });
-                this.setState({searchItem:updatedResponse})
-            })
-        }
+                }
+                return book
+            });
+            this.setState({searchItem:updatedResponse})
+        })
     }
 
     onShelfChangeHandler = (book, shelf) => {
         if(shelf==="move") return
         bookApi.update(book, shelf).then((res) => {
             let shelfHistory = JSON.parse(localStorage.getItem('shelfHistory'));
+			let isNewBook = false
             for(var count = 0; count < shelfHistory.length; count++){
-                let shelfType='none'
+                
                 if(book.id === shelfHistory[count].id){
-                    shelfType=shelf;   
+                    shelfHistory[count].shelf=shelf;  
+					isNewBook = true
                 }
-                shelfHistory[count].shelf=shelfType
+                
             }
+			if(isNewBook){
+				shelfHistory.push({
+                    id:book.id,
+                    shelf:shelf
+                })
+			}
             localStorage.setItem('shelfHistory', JSON.stringify(shelfHistory))
         })
     }
